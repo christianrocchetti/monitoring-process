4a1958267222430a441c1a649f039a3feb9ce8b1
chri <rocchettichristian@gmail.com>
2021-02-19 15:58:36 +0100

    init project

---
+++ installer.sh
@@ -0,0 +1,15 @@
+sudo cp monitoring-process.service /etc/systemd/system/
+sudo cp monitoring-process /usr/bin/
+
+sudo chmod +x /usr/bin/monitoring-process
+
+sudo mkdir -p /var/log/monitoring-process/
+
+# Arg default
+sudo mkdir -p /etc/monitoring-process/
+sudo cp .argconf /etc/monitoring-process/.argconf
+
+sudo systemctl daemon-reload
+sudo systemctl stop monitoring-process.service
+sudo systemctl start monitoring-process.service
+sudo systemctl status monitoring-process.service
